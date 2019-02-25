df394c5d1710bf2f369c4662d6ba4b0a17208046
dejicranium <itisdeji@gmail.com>
2019-02-25 13:28:05 +0100

    wer

---
+++ greggo/sample.py
@@ -0,0 +1,28 @@
+import redis
+from greggo.config import REDIS_SERVER
+
+r = REDIS_SERVER
+def convert_dictt_to_list(dictt):
+    return [k for k in dictt.keys()]
+def add_something(something):
+    key = "abc"
+    count = r.scard(key)
+    members = r.smembers(key)
+    add = r.sadd
+    remove = r.srem
+    if count == 5 or count > 5:
+        members_ = [k for k in members]
+        length = len(members_)
+        last_member = members_[length-1]
+        remove(key, last_member)
+        add(key, something)
+    else: 
+        add(key, something)
+    print(members)
+if __name__ == "__main__":
+    for i in range(0, 100):
+<<<<<<< HEAD
+        r.spop('u_fd:' + str(i))
+=======
+        r.spop('u_fd:' + i)
+>>>>>>> 4fe121ecaea842da72103973e170b5179bc1f8fa
