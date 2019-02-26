4908cb5a140613df76fcd2eb0cbdc7110a401dc7
dejicranium <itisdeji@gmail.com>
2019-02-26 10:53:26 +0100

    wer

--- repoll/services/feed_service.py
+++ repoll/services/feed_service.py
@@ -40,11 +40,11 @@ def get_activities_if_authenticated(request, user, page):
         for category in categories:
             user_categories.append(category.id)
 
-    activities = FeedManager(user.id).get_all_feeds(user_categories)    
+    activities = FeedManager(user.id).get_all_feeds(user_categories)
+    paginator = SqlalchemyOrmPage(activities, page=page, items_per_page=15, item_count=len(activities))
+    
     #activities = get_latest_activities(request, user.id, already_shown)
     activities = request.dbsession.query(Activity).filter(Activity.id.in_(activities))
-    paginator = SqlalchemyOrmPage(activities, page=page, items_per_page=15, item_count=len(activities))
-    activities = paginator.items
 
     for activity in activities:
         source = get_source(request, activity)
