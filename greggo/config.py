c47c6d26e91de743706c14be633a3c0c3075ab6b
Deji Atoyebi <itisdeji@gmail.com>
2019-03-11 11:51:10 +0100

    thou shall not kill

--- greggo/config.py
+++ greggo/config.py
@@ -1,8 +1,8 @@
 import redis
 
-#EDIS_SERVER = redis.StrictRedis(host="pozet-redis.0jy3so.ng.0001.use2.cache.amazonaws.com", port="6379", db=0)
+REDIS_SERVER = redis.StrictRedis(host="pozet-redis.0jy3so.ng.0001.use2.cache.amazonaws.com", port="6379", db=0)
 
-REDIS_SERVER = redis.Redis()
+#REDIS_SERVER = redis.Redis()
 
 
 def change_redis_connection():
